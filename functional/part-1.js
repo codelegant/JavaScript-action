bb056fd604efc77ae6a241a338012521fadb2e83
Laichuanfeng <elegant.lai@kiwifi.cn>
2016-02-17 20:14:39 +0800

    添加：函数式编辑

---
+++ functional/part-1.js
@@ -0,0 +1,29 @@
+var ponies = [
+    [
+        ['name', 'Fluttershy'],
+        ['image', 'http://tinyurl.com/gpbnlf6'],
+        ['description', 'Fluttershy is a female Pegasus pony and one of the main characters of My Little Pony Friendship is Magic.']
+    ],
+    [
+        ['name', 'Applejack'],
+        ['image', 'http://tinyurl.com/gkur8a6'],
+        ['description', 'Applejack is a female Earth pony and one of the main characters of My Little Pony Friendship is Magic.']
+    ],
+    [
+        ['name', 'Twilight Sparkle'],
+        ['image', 'http://tinyurl.com/hj877vs'],
+        ['description', 'Twilight Sparkle is the primary main character of My Little Pony Friendship is Magic.']
+    ]
+];
+function addToObject(obj,arr){
+    obj[arr[0]]=arr[1];
+    return obj;
+}
+var reduce = function(callback, initialValue, array) {
+    var working = initialValue;
+    for (var i = 0; i < array.length; i = i + 1) {
+        working = callback(working, array[i]);
+    }
+    return working;
+};
+console.log(reduce(addToObject,{},ponies));
---
+++ functional/part-3.js
@@ -0,0 +1,63 @@
+var showArgs = function (a, b) {
+    var args = Array.prototype.slice.call(arguments, 0);
+    console.log(args);
+}
+//showArgs('Tweedledee', 'Tweedledum', 'b', 'c', 'd');
+
+function log(name) {
+    console.log(name);
+}
+//log.call(null, 'lai');
+
+var animals = [{
+    species: 'Lion',
+    name: 'King'
+}, {
+        species: 'Whale',
+        name: 'Fail'
+    }];
+
+for (var i = 0; i < animals.length; i++) {
+    (function (i) {
+        this.print = function () {
+            var str = '#' + i + ' ' + this.species + ': ' + this.name;
+            //console.log(str);
+        }
+        this.print();
+    }).call(animals[i], i);
+}
+
+var getElementById = function (id) {
+    var ele = {};
+    ele.className = id + '-class';
+    return ele;
+}
+var addClass = function (className, element) {
+    element.className += ' ' + className;
+    return element;
+}
+var addTweedleClass = function (el) {
+    return addClass('tweedle', el);
+}
+var ids = ['DEE', 'DUM'];
+//var elements=ids.map(getElementById);
+var elements = ids.map(getElementById);
+elements = elements.map(addTweedleClass);
+console.log(elements);
+function partial() {
+    var origin=arguments;
+    var fn = Array.prototype.shift.apply(arguments);
+    return function () {
+        console.log(arguments);
+        return fn.apply(this, origin);
+    }
+}
+var twinkle = function(noun, wonderAbout) {
+    return 'Twinkle, twinkle, little ' +
+        noun + '\nHow I wonder where you ' +
+        wonderAbout;
+}
+
+var twinkleBat = partial(twinkle, 'bat', 'are at');
+var twinkleStar = partial(twinkle, 'star', 'are');
+console.log(twinkleBat());
