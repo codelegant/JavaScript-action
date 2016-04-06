38f3042ca47a5f1433677b52bb78eac47f03f788
codelegant <laichuanfeng@hotmail.com>
2016-04-06 21:13:06 +0800

    修改：代码格式化

--- async-javascript/setTimeout-1.js
+++ async-javascript/setTimeout-1.js
@@ -2,7 +2,7 @@
 // 	setTimeout(function(){console.log(i)});//输出1，2，3
 // }
 for (var i = 1; i <= 3; i++) {
-	//setTimeout(function(){console.log(i)});//输出4，4，4
+  //setTimeout(function(){console.log(i)});//输出4，4，4
 }
 
 //JavaScript 事件处理器在线程空闲之前不会运行。
--- async-javascript/worker.js
+++ async-javascript/worker.js
@@ -1,5 +1,5 @@
 (function () {
-	self.addEventListener("message", function (e) {
-		self.postMessage("Bo knows" + e.data);
-	});
+  self.addEventListener("message", function (e) {
+    self.postMessage("Bo knows" + e.data);
+  });
 })();
